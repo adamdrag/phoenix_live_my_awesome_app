486814b7ad20ed39ca9d248903d4a619c8a178fb
Adam Drag <adrag@GL-MO-LT-00005.local>
2021-01-13 15:33:50 -0700

    fixed navigation dropdown and mobile menu view

--- lib/my_awesome_app_web/live/view/header_live/index.ex
+++ lib/my_awesome_app_web/live/view/header_live/index.ex
@@ -1,8 +1,31 @@
 defmodule MyAwesomeAppWeb.View.HeaderLive.Index do
-  # use Phoenix.LiveComponent
   use MyAwesomeAppWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, number: 5)}
+  def mount(_params, session, socket) do
+    %{"current_user" => current_user} = session
+
+    socket =
+      assign(socket,
+        current_user: current_user,
+        hamburger_icon: "block",
+        toggle_hamburger_menu: "hidden",
+        toggle_user_menu: "hidden"
+      )
+
+    {:ok, socket}
+  end
+
+  def handle_event("toggle-hamburger-menu", _, socket) do
+    socket =
+      socket
+      |> update(:toggle_hamburger_menu, &if(&1 == "hidden", do: "block", else: "hidden"))
+      |> update(:hamburger_icon, &if(&1 == "block", do: "hidden", else: "block"))
+
+    {:noreply, socket}
+  end
+
+  def handle_event("toggle-user-menu", _, socket) do
+    socket = update(socket, :toggle_user_menu, &if(&1 == "hidden", do: "block", else: "hidden"))
+    {:noreply, socket}
   end
 end
