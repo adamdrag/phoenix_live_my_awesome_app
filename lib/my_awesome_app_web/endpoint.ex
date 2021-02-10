5eeb8c72ceb866a47ce6be617e4c9f44dc98b0ea
Adam Drag <adrag@GL-MO-LT-00005.local>
2021-02-10 13:37:40 -0700

    added updating avatar image

--- lib/my_awesome_app_web/endpoint.ex
+++ lib/my_awesome_app_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule MyAwesomeAppWeb.Endpoint do
     at: "/",
     from: :my_awesome_app,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js uploads favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
