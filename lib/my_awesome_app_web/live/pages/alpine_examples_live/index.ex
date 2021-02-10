242e268748e4ddbd5f35c56e292f2f8eabfccfa9
Adam Drag <adrag@GL-MO-LT-00005.local>
2021-02-10 14:38:10 -0700

    added in alpine examples page modal

--- lib/my_awesome_app_web/live/pages/alpine_examples_live/index.ex
+++ lib/my_awesome_app_web/live/pages/alpine_examples_live/index.ex
@@ -9,6 +9,10 @@ defmodule MyAwesomeAppWeb.Pages.AlpineExamplesLive.Index do
     ~L"""
       <%= live_component @socket, MyAwesomeAppWeb.Components.PageTitleLive, title: "Alpine Examples" %>
 
+      <!--DropDown-->
+      <div class="mt-12 mb-4">
+        <%= render(MyAwesomeAppWeb.ComponentView, "text_in_border_line.html", text: "Drop Down", bg_color: "bg-gray-200" ) %>
+      </div>
       <div class="flex justify-center">
         <div
           id="<%= if connected?(@socket), do: 'connected-id', else: 'not-connected-id' %>"
@@ -39,7 +43,7 @@ defmodule MyAwesomeAppWeb.Pages.AlpineExamplesLive.Index do
               x-transition:leave="transition ease-in duration-75"
               x-transition:leave-start="transform opacity-100 scale-100"
               x-transition:leave-end="transform opacity-0 scale-95"
-              class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg"
+              class="z-10 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg"
             >
               <div class="rounded-md bg-white shadow-xs">
                   <div class="py-1">
@@ -54,6 +58,48 @@ defmodule MyAwesomeAppWeb.Pages.AlpineExamplesLive.Index do
             </div>
         </div>
       </div>
+
+      <!--Modal-->
+      <div class="mt-12 mb-4">
+        <%= render(MyAwesomeAppWeb.ComponentView, "text_in_border_line.html", text: "Modal", bg_color: "bg-gray-200" ) %>
+      </div>
+      <div
+        id="modal-alpine"
+        x-data="{ 'showModalTest': false }"
+        @keydown.escape="showModalTest = false"
+        @click.away="showModalTest = false"
+      >
+        <div class="flex items-center justify-center my-6">
+          <button
+            @click="showModalTest = true"
+            class="bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full"
+          >
+            Open Modal
+          </button>
+        </div>
+        <div
+          x-show="showModalTest"
+          x-transition:enter="transition ease-out duration-100"
+          x-transition:enter-start="transform opacity-0 scale-95"
+          x-transition:enter-end="transform opacity-100 scale-100"
+          x-transition:leave="transition ease-in duration-75"
+          x-transition:leave-start="transform opacity-100 scale-100"
+          x-transition:leave-end="transform opacity-0 scale-95"
+          class="modal opacity 0.25s ease pointer-events-none absolute w-full h-full top-0 left-0 flex items-center justify-center"
+        >
+          <div @click.away="showModalTest = false" class="modal-overlay absolute w-full h-full bg-black opacity-25 top-0 left-0 cursor-pointer"></div>
+            <div class="absolute w-1/2 h-32 bg-white rounded-sm shadow-lg flex items-center justify-center text-2xl">
+              <p>Modal content can go here</p>
+            </div>
+        </div>
+      </div>
+
+      <!--Drag and Drop File Upload-->
+      <div class="mt-12 mb-4">
+        <%= render(MyAwesomeAppWeb.ComponentView, "text_in_border_line.html", text: "Drag and Drop File Upload", bg_color: "bg-gray-200" ) %>
+      </div>
+
+
     """
   end
 end
