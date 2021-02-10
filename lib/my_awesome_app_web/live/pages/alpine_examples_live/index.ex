d1a18c578be6b0cf801dcf1df6a40752ffa80bfa
Adam Drag <adrag@GL-MO-LT-00005.local>
2021-02-10 14:54:21 -0700

    added alpine modal for setting avatar image

--- lib/my_awesome_app_web/live/pages/alpine_examples_live/index.ex
+++ lib/my_awesome_app_web/live/pages/alpine_examples_live/index.ex
@@ -65,20 +65,20 @@ defmodule MyAwesomeAppWeb.Pages.AlpineExamplesLive.Index do
       </div>
       <div
         id="modal-alpine"
-        x-data="{ 'showModalTest': false }"
-        @keydown.escape="showModalTest = false"
-        @click.away="showModalTest = false"
+        x-data="{ 'showModal': false }"
+        @keydown.escape="showModal = false"
+        @click.away="showModal = false"
       >
         <div class="flex items-center justify-center my-6">
           <button
-            @click="showModalTest = true"
+            @click="showModal = true"
             class="bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full"
           >
             Open Modal
           </button>
         </div>
         <div
-          x-show="showModalTest"
+          x-show="showModal"
           x-transition:enter="transition ease-out duration-100"
           x-transition:enter-start="transform opacity-0 scale-95"
           x-transition:enter-end="transform opacity-100 scale-100"
@@ -87,10 +87,10 @@ defmodule MyAwesomeAppWeb.Pages.AlpineExamplesLive.Index do
           x-transition:leave-end="transform opacity-0 scale-95"
           class="modal opacity 0.25s ease pointer-events-none absolute w-full h-full top-0 left-0 flex items-center justify-center"
         >
-          <div @click.away="showModalTest = false" class="modal-overlay absolute w-full h-full bg-black opacity-25 top-0 left-0 cursor-pointer"></div>
-            <div class="absolute w-1/2 h-32 bg-white rounded-sm shadow-lg flex items-center justify-center text-2xl">
-              <p>Modal content can go here</p>
-            </div>
+          <div @click.away="showModal = false" class="modal-overlay absolute w-full h-full bg-black opacity-25 top-0 left-0 cursor-pointer"></div>
+          <div class="absolute w-1/2 h-32 bg-white rounded-sm shadow-lg flex items-center justify-center text-2xl">
+            <p>Modal content can go here</p>
+          </div>
         </div>
       </div>
 
